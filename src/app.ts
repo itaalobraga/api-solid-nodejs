feb495699f6dc11711af2aaffb512fa46b9a56a6
Ítalo Braga <itojk2@hotmail.com>
2023-03-20 20:08:45 -0300

    feat: create user

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,6 @@
-import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
 
-const prisma = new PrismaClient();
-
-prisma.user.create({
-  data: {
-    name: "Ítalo Braga",
-    email: "test.com",
-  },
-});
+app.register(appRoutes);
