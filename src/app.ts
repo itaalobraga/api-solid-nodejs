857b519c1b1f085e7c54584aa3a04760b9dd1970
Ítalo Braga <itojk2@hotmail.com>
2023-05-06 14:11:29 -0300

    feat: refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import fastifyCookie from "@fastify/cookie";
 import fastifyJwt from "@fastify/jwt";
 import fastify from "fastify";
 import { ZodError } from "zod";
@@ -8,7 +9,16 @@ export const app = fastify();
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "120",
+  },
 });
+
+app.register(fastifyCookie);
 
 app.register(appRoutes);
 
